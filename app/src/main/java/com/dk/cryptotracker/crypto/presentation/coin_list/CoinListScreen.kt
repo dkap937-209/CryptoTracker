2b3d4b8d45c0e601e6e181352229f67662aa40fc
Darpan <darpan.kapadia@gmail.com>
2024-10-25 20:37:28 +1300

    Created Coin List Screen Ui

---
+++ app/src/main/java/com/dk/cryptotracker/crypto/presentation/coin_list/CoinListScreen.kt
@@ -0,0 +1,69 @@
+package com.dk.cryptotracker.crypto.presentation.coin_list
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.dk.cryptotracker.crypto.presentation.coin_list.components.CoinListItem
+import com.dk.cryptotracker.crypto.presentation.coin_list.components.previewCoin
+import com.dk.cryptotracker.ui.theme.CryptoTrackerTheme
+
+@Composable
+fun CoinListScreen(
+    modifier: Modifier = Modifier,
+    state: CoinListState
+) {
+
+    if(state.isLoading) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            CircularProgressIndicator()
+        }
+    }
+    else {
+        LazyColumn(
+            modifier = modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(state.coins) { coinUi ->
+                CoinListItem(
+                    modifier = Modifier.fillMaxSize(),
+                    coinUi = coinUi,
+                    onClick = { /*TODO*/ }
+                )
+                HorizontalDivider()
+            }
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun CoinListScreenPreview() {
+    CryptoTrackerTheme {
+        CoinListScreen(
+            state = CoinListState(
+                coins = (1.. 100).map {
+                    previewCoin.copy(id = it.toString())
+                }
+            ),
+            modifier = Modifier.background(MaterialTheme.colorScheme.background)
+        )
+    }
+}
